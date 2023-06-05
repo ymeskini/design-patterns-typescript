f176d61fb63055055da47c893413b2119a145c8a
Youssef Meskini <youss.meskini@gmail.com>
2023-06-05 17:27:11 +0100

    add rxjs websocket observer pattern example with twitch chat

--- src/observer/twitch-chat/index.ts
+++ src/observer/twitch-chat/index.ts
@@ -1,6 +1,25 @@
-import { filter, map, scan } from 'rxjs';
+import { randomUUID } from 'crypto';
+import {
+  Subject,
+  auditTime,
+  delay,
+  filter,
+  interval,
+  map,
+  scan,
+  startWith,
+  tap,
+} from 'rxjs';
 import { webSocket } from 'rxjs/webSocket';
-import WebSocket from 'ws';
+import WebSocket, { WebSocketServer } from 'ws';
+
+declare module 'ws' {
+  interface WebSocket {
+    id?: string;
+  }
+}
+
+const wss = new WebSocketServer({ port: 8080 });
 
 const TWITCH_URL = 'wss://irc-ws.chat.twitch.tv:443';
 const TWITCH_COMMANDS = {
@@ -21,10 +40,14 @@ const initChat = (channelName: string) => {
   socket$.next('NICK justinfan12345');
   socket$.next(`JOIN #${channelName}`);
 
-  socket$.pipe(filter((msg) => msg === TWITCH_COMMANDS.PING)).subscribe(() => {
-    console.log('PING');
-    socket$.next(TWITCH_COMMANDS.PONG);
-  });
+  socket$
+    .pipe(
+      //   tap(console.log),
+      filter((msg) => msg === TWITCH_COMMANDS.PING),
+    )
+    .subscribe(() => {
+      socket$.next(TWITCH_COMMANDS.PONG);
+    });
 
   return {
     $messages: socket$.pipe(
@@ -37,13 +60,59 @@ const initChat = (channelName: string) => {
   };
 };
 
-const { $messages } = initChat('waolol1');
-const $messagesCount = $messages.pipe(scan((acc) => acc + 1, 0));
+const channelName = process.argv[2];
+const { $messages } = initChat(channelName);
+const countSubject$ = new Subject<number>();
+const $messagesCount = countSubject$.pipe(
+  startWith(0),
+  scan((acc) => acc + 1),
+);
 
-$messages.subscribe((message) => {
-  console.log(message);
+$messages.subscribe(() => {
+  countSubject$.next(1);
 });
 
-$messagesCount.subscribe((count) => {
-  console.log('count', count);
+const clientConnections = new Map<string, boolean>();
+
+wss.on('connection', (ws) => {
+  ws.id = randomUUID();
+  clientConnections.set(ws.id, true);
+
+  const pingSub = interval(20000)
+    .pipe(
+      tap(() => {
+        ws.send('PING');
+        clientConnections.set(ws.id, false);
+      }),
+      delay(20000),
+      filter(() => !clientConnections.get(ws.id)),
+      tap(() => {
+        ws.close();
+      }),
+    )
+    .subscribe();
+
+  const countSubscription = $messagesCount
+    .pipe(auditTime(10000))
+    .subscribe((count) => {
+      ws.send(`Count: ${count}`);
+    });
+
+  const messagesSubscription = $messages.subscribe((message) => {
+    ws.send(message);
+  });
+
+  ws.on('message', (message) => {
+    if (message.toString() === 'PONG') {
+      clientConnections.set(ws.id, true);
+    }
+  });
+
+  ws.on('close', () => {
+    console.log(`Client ${ws.id} disconnected`);
+    pingSub.unsubscribe();
+    countSubscription.unsubscribe();
+    messagesSubscription.unsubscribe();
+    clientConnections.delete(ws.id);
+  });
 });
